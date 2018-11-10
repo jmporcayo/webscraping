5101a2d834a7906db2ea4da0c84adb2a0aebd238
JMP <porcayojm@gmail.com>
2018-11-10 13:46:51 -0800

    updated port in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 
 var routes = require("./routes");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
